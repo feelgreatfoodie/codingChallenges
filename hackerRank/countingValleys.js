11b0b65e1dfcee96621d550d94c3baafd9865725
Christian Bourlier <chrisbourlier@hotmail.com>
2018-10-08 11:08:42 -0600

    reduce, object.entries, map

---
+++ hackerRank/countingValleys.js
@@ -0,0 +1,23 @@
+
+const n = 8
+const s = 'UDDDUDUU'
+
+function countingValleys(n, s) {
+  //starting position of sea level is 0
+  //any time current position reaches 0 from -n, increment valley countingValleys
+  //disregard mountains
+  let position = 0
+  let valleys = 0
+
+  s.split('').map(e => {
+    if(e === 'D') --position
+    else {
+      ++position
+      if (position === 0) valleys++
+    }
+  })
+
+  return valleys
+}
+
+countingValleys(n, s)
---
+++ hackerRank/sockMerchant.js
@@ -0,0 +1,15 @@
+// Complete the sockMerchant function below.
+const n = 9
+const ar = [ 10, 20, 20, 10, 10, 30, 50, 10, 20 ]
+
+function sockMerchant(n, ar) {
+    let sockPairs = 0
+    let obj = ar.reduce((acc, cv) => {
+        acc[cv] ? acc[cv]++ : acc[cv] = 1
+        return acc
+    }, {})
+    Object.entries(obj).map(e => sockPairs += Math.floor(e[1]/2))
+    return sockPairs
+}
+
+console.log(sockMerchant(n,ar))
